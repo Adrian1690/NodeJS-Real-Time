ad4cd336d9ac68f57ffad9857b323f7782f21391
Adrian1690 <j.adrian.1690@gmail.com>
2015-02-18 00:16:57 -0500

    Autentication

--- blog/app.js
+++ blog/app.js
@@ -9,10 +9,10 @@ var mongoskin = require('mongoskin'),
       users : db.collection('users')	
     };
 
-var session = require('express-session'),
-    logger = require('morgan'),
+var logger = require('morgan'),
     errorHandler = require('errorhandler'),
     cookieParser = require('cookie-parser'),
+    session = require('express-session'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override');
 
@@ -26,6 +26,24 @@ app.use(function(req, res, next){
   return next ();
 });
 
+// middlewar for auth
+app.use(function(req, res, next){
+  if(req.session && req.session.admin)
+    res.locals.admin = true;
+  next();
+});
+
+// Authorization
+
+var authorize = function (req, res, next){
+  if(req.session && req.session.admin)
+    return next();
+  else
+    return res.send(401);//no autorized
+    //res.render('login' ,{error : "Introduce your email and password file"});
+    
+};
+
 // define port view and view engine
 app.set('port', process.env.PORT || 3000 );
 app.set('views', path.join(__dirname , 'views'));
@@ -38,6 +56,10 @@ app.use(methodOverride());
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname,'public')));
 
+// Implmenets auth
+app.use(cookieParser('2CCC4ACD-6ED1-4844-9217-82131BDCB239'));
+app.use(session({secret: '1C44774A-D649-4D44-9535-46E296EF984F'}));
+
 // development only
 if('development' === app.get('env')){
   app.use(errorHandler());
@@ -45,20 +67,34 @@ if('development' === app.get('env')){
 
 // pages and routes
 
+/* app.get('/pruebas',function(req,res){
+  db.collection("users",function(err,collection){
+    collection.find().toArray(function(err, docs) {
+          console.log("Printing docs from Array")
+          docs.forEach(function(doc) {
+            console.log("Doc from Array ");
+            console.dir(doc);
+          });
+    });
+ });
+});
+*/
+
 app.get('/',routes.index);
 app.get('/login',routes.user.login);
 app.post('/login',routes.user.authenticate);
-app.get('/login',routes.user.logout);
-app.get('/admin',routes.article.admin);
-app.get('/post',routes.article.post);  
-app.post('/post',routes.article.postArticle);
+app.get('/logout',routes.user.logout);
+app.get('/admin', authorize, routes.article.admin);
+app.get('/post', authorize, routes.article.post);  
+app.post('/post', authorize, routes.article.postArticle);
 app.get('/articles/:slug',routes.article.show);
 
 // rest api routes
-app.get('/api/articles',routes.article.list);
-app.post('/api/articles',routes.article.add);
-app.put('/api/articles/:id',routes.article.edit);
-app.del('/api/articles/:id',routes.article.del);
+app.get('api', authorize);
+app.get('/api/articles', routes.article.list);
+app.post('/api/articles', routes.article.add);
+app.put('/api/articles/:id', routes.article.edit);
+app.del('/api/articles/:id', routes.article.del);
 
 
 // now middlewar
--- blog/routes/user.js
+++ blog/routes/user.js
@@ -13,11 +13,24 @@ exports.login = function(req, res, next){
 //get logout route
 
 exports.logout = function(req, res, next){
+  req.session.destroy();
   res.redirect('/');
 };
 
 // post authenticate route
 
 exports.authenticate = function (req, res , next){
-  res.redirect('/admin');
+  console.log( 'email:' + req.body.email + ' pass:' +req.body.password);
+  if ( !req.body.email || !req.body.password)
+    return res.render('login', {error : "Email or Password is empty"});
+  req.collections.users.findOne({
+    email : req.body.email,
+    password : req.body.password
+  }, function (error, user){
+    if (error) return next(error);
+    if (!user) return res.render('login' ,{error : "Incorrect email&password combination"});
+    req.session.user = user;
+    req.session.admin = user.admin;
+    res.redirect('/admin');
+  })
 };
